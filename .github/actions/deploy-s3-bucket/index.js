3598723dfc29a484a1091d3a123511914950e8a5
Jyoti Yadav <jyotiyadav282003@gmail.com>
2026-07-23 14:20:50 +0530

    Static site deployemnt

--- .github/actions/deploy-s3-bucket/index.js
+++ .github/actions/deploy-s3-bucket/index.js
@@ -4,6 +4,15 @@ import * as github from '@actions/github';
 // const exec = require('@actions/exec')
 
 function run(){
+    // Get the input values
+    const bucket = core.getInput('bucket' , {required: true});
+    const bucketRegion = core.getInput('bucket-region' , {required: true});
+    const distFolder = core.getInput('dist-folder' , {required: true});
+
+    // upload the files
+    const s3Uri = `s3://${bucket}` ;
+    exec.exec(`aws sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
     core.notice("hello from Custom JavaScript action")
 }
 
