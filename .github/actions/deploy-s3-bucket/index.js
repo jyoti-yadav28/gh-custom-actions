f86b0047391fba2cd1cf0fc93a3eec6a8abe73f3
Jyoti Yadav <jyotiyadav282003@gmail.com>
2026-07-23 13:12:53 +0530

    changes added

--- .github/actions/deploy-s3-bucket/index.js
+++ .github/actions/deploy-s3-bucket/index.js
@@ -1,6 +1,7 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
-const exec = require('@actions/exec')
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+// const github = require('@actions/github')
+// const exec = require('@actions/exec')
 
 function run(){
     core.notice("hello from Custom JavaScript action")
